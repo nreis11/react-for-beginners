5d4797bcf80b045337bcd6d78f3dce8b4ad8bced
reeses-pieces <nicholasreis@icloud.com>
2017-03-21 15:44:50 -0700

    add createFish func

--- catch-of-the-day/src/components/AddFishForm.js
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -4,18 +4,27 @@ class AddFishForm extends React.Component {
   createFish(event) {
     event.preventDefault();
     console.log("Making a fish!");
+
+    const fish = {
+      name: this.name.value,
+      price: this.price.value,
+      status: this.status.value,
+      desc: this.desc.value,
+      image: this.image.value
+    }
+    console.log(fish);
   }
   render() {
     return (
       <form className="fish-edit" onSubmit={(e) => this.createFish(e)}>
-        <input type="text" placeholder="Fish name"/>
-        <input type="text" placeholder="Fish price"/>
-        <select>
+        <input ref={(input) => this.name = input} type="text" placeholder="Fish name"/>
+        <input ref={(input) => this.price = input} type="text" placeholder="Fish price"/>
+        <select ref={(input) => this.status = input}>
           <option value="available">Fresh!</option>
           <option value="unavailable">Sold Out!</option>
         </select>
-        <textarea placeholder="Fish Desc"></textarea>
-        <input type="text" placeholder="Fish Image"/>
+        <textarea ref={(input) => this.desc = input} placeholder="Fish Desc"></textarea>
+        <input ref={(input) => this.image = input} type="text" placeholder="Fish Image"/>
         <button type="submit">+ Add Item</button>
       </form>
     )
