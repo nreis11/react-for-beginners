3112958ad3254dd0ab5e1e2946344746d651cb69
reeses-pieces <nicholasreis@icloud.com>
2017-03-20 18:24:05 -0700

    create router, add matches and misses

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,9 +1,24 @@
 import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter, Match, Miss } from "react-router";
 import StorePicker from "./components/StorePicker";
 import App from "./components/App";
+import NotFound from "./components/NotFound";
 import './css/style.css'
 
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker} />
+        <Match pattern="/store/:storeId" component={App} />
+        {/* If there's no match */} 
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
+  )
+}
 
 
-render(<App/>, document.querySelector("#main"))
+render(<Root/>, document.querySelector("#main"))
+
