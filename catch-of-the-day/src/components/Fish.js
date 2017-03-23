fed6a7ef48310142456659033e819f06103e150b
reeses-pieces <nicholasreis@icloud.com>
2017-03-22 19:04:22 -0700

    minor var fix

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -7,7 +7,7 @@ class Fish extends React.Component {
     return (
       <li className="menu-fish">
         <img src={details.image} alt={details.name}/>
-        {this.props.details.name}
+        {details.name}
         <h3 className="fish-name">
           {details.name}
           <span className="price">{formatPrice(details.price)}</span>
