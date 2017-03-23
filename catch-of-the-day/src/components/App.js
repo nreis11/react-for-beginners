fd7e549c0158e15ca894184a806232ad74a7e7a4
reeses-pieces <nicholasreis@icloud.com>
2017-03-23 15:10:12 -0700

    add hook up AddToOrder func to add to order button

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -11,7 +11,8 @@ class App extends React.Component {
 
     // Set reference to App in AddFish method
     this.addFish = this.addFish.bind(this);
-    this.loadSamples = this.loadSamples.bind(this)
+    this.loadSamples = this.loadSamples.bind(this);
+    this.addToOrder = this.addToOrder.bind(this);
 
     // getInitialState
     this.state = {
@@ -36,6 +37,15 @@ class App extends React.Component {
     });
   }
 
+  addToOrder(key) {
+    // take a copy of order state
+    const order = {...this.state.order};
+    // update or add the new number of fish ordered
+    order[key] = order[key] + 1 || 1;
+    // update our state
+    this.setState({ order: order })
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -45,7 +55,7 @@ class App extends React.Component {
             {
               Object
                 .keys(this.state.fishes)
-                .map(key => <Fish key={key} details={this.state.fishes[key]} />)
+                .map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />)
             }
           </ul>
         </div>
