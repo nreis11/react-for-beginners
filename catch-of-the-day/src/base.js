04ff51466470a30348a91a339b4ef2fa7eaf7ceb
reeses-pieces <nicholasreis@icloud.com>
2017-03-23 17:47:01 -0700

    add firebase connection

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyBjaBH9jUhllRCOCMvdT2sQ6yAu3EKV4mw",
+    authDomain: "catch-of-the-day-nick-f53f0.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-nick-f53f0.firebaseio.com",
+});
+
+export default base;
